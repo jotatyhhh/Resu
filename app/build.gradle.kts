63eb03cfbe9b188efb2de7d687f5b18ad5b6ac13
jotatyhhh <mateusjuan2508@gmail.com>
2026-08-16 03:34:42 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.example.dpisimulator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.dpisimulator"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+}
